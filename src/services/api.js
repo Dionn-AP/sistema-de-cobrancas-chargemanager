a10d2643178328f3b6c47bea84e2c4f1d17e5fdd
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-07 15:40:05 -0300

    alterando o endereço de requisição dao servidor

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://desafio-modulo05.herokuapp.com',
+    baseURL: 'https://charge-manager-server.onrender.com',
     timeout: 100000,
     headers: { 'Content-Type': 'application/json' }
 });
